4c3cc1db817b00cf60e426bc44d0e71188748b8b
kimji <jhkim@youngjintech.com>
2025-01-06 16:41:27 +0900

    인터페이스 타입 적용 해보기

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,18 @@
 import './style/App.css'
 
+interface ButtonProps {
+  text: string,
+  onClick: ()=> void;
+}
+
+const Button: React.FC<ButtonProps> = ({ text, onClick }) => (
+    <button onClick={onClick}>{text}</button>
+)
+
 function App() {
 
+
+  //타입선언
   const add = (a:number, b:number):number => {
     return a + b;
   }
@@ -12,12 +23,19 @@ function App() {
     return parseInt(a)+parseInt(b);
   }
 
+  //인터페이스 타입
+  const consoles = () => {
+    return console.log('hello world');
+  }
+
 
   return (
       <div>
         {add(1, 2)} <br/>
         {stringAdd("안녕", "하세요")} <br/>
         {stringIntAdd("3", "4")} <br/>
+
+        <Button text="하이하이" onClick={consoles}/>
       </div>
   )
 }
