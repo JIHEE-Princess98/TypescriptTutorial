41f3a87beaece118b9621111598c4809ce8ec104
kimji <jhkim@youngjintech.com>
2025-01-06 16:36:17 +0900

    type 별 연산함수

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,23 @@ import './style/App.css'
 
 function App() {
 
-  return (
-    <div>
+  const add = (a:number, b:number):number => {
+    return a + b;
+  }
+  const stringAdd = (a:string, b:string):string => {
+    return a+b;
+  }
+  const stringIntAdd = (a:string, b:string):number => {
+    return parseInt(a)+parseInt(b);
+  }
 
-    </div>
+
+  return (
+      <div>
+        {add(1, 2)} <br/>
+        {stringAdd("안녕", "하세요")} <br/>
+        {stringIntAdd("3", "4")} <br/>
+      </div>
   )
 }
 
